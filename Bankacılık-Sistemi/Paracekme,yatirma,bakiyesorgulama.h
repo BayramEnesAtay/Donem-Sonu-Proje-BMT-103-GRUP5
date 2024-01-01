4726432d1ffce042f589d0472d4df59c7c421da7
Abdülselam_İsmail <147073125+Abdulselam23181616010@users.noreply.github.com>
2024-01-01 17:31:49 +0300

    Update Paracekme,yatirma,bakiyesorgulama.h

--- Bankacılık-Sistemi/Paracekme,yatirma,bakiyesorgulama.h
+++ Bankacılık-Sistemi/Paracekme,yatirma,bakiyesorgulama.h
@@ -1,18 +1,22 @@
 //Muhammed Ali Atik 23181616601
 //Para yatirma,cekme, bakiye sorgulama.
 
+//Muhammed Ali Atik 23181616601
+//Para yatirma,cekme, bakiye sorgulama.
+
 #include<stdio.h>
 #include<stdlib.h>
 
-int BakiyeSorgulama(char *kullanici) {
+void BakiyeSorgulama(char kullanici[50]) {
     char dosya_adi[100];
 
 
-    snprintf(dosya_adi, sizeof(dosya_adi), "%s.txt", *kullanici);
+    snprintf(dosya_adi, sizeof(dosya_adi), "%s", kullanici);
     FILE *dosya = fopen(dosya_adi, "r");
     if (dosya == NULL) {
+        printf(dosya_adi);
         printf("Dosya acilamadi!\n");
-        return 0;
+
     }
 
     int bakiye;
@@ -26,42 +30,5 @@ int BakiyeSorgulama(char *kullanici) {
     }
     fclose(dosya);
 
-    return bakiye;
-}
-void Paracekme(int*aptr) {
-    int para;
-    int secim;
 
-    printf("Para Yatirma/Cekme islemlerinden hangisini yapmak istiyorsaniz giriniz(Yatirma icin 1,Cekme icin 0):");
-    scanf("%d", &secim);
-    switch (secim) {
-        case 1: {
-            printf("Yatirmak istediginiz parayi giriniz:");
-            scanf("%d", &para);
-            *aptr += para;
-            printf("Paraniz basarili bir sekilde yatirilmistir.\n");
-            printf("Hesabinizda %d toplam para vardir.", *aptr);
-            break;
-        }
-        case 0: {
-            printf("Cekmek istediginiz parayi giriniz:");
-            scanf("%d", &para);
-
-            // Bakiye sorgulama fonksiyonu
-            int bakiye = BakiyeSorgulama(aptr);
-
-            // Bakiye kontrolü
-            if (bakiye >= para) {
-                *aptr -= para;
-                printf("Paraniz basarili bir sekilde cekilmistir!\n");
-                printf("Hesabinizda %d kadar toplam para vardir", *aptr);
-            } else {
-                printf("Hesabinizda yeterli bakiye bulunmamaktadir!\n");
-            }
-            break;
-
-            default:
-                printf("Gecersiz secim yaptiniz lutfen 0 veya 1 giriniz!:\n");
-        }
-    }
 }
