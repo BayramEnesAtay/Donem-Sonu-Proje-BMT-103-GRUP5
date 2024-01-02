41aff5c3e3a0d541e9ceba6f29c3982989f31977
Abdülselam_İsmail <147073125+Abdulselam23181616010@users.noreply.github.com>
2024-01-02 15:45:32 +0300

    Update gecmis.h
    1.Yorumlar eklendi

--- Bankacılık-Sistemi/gecmis.h
+++ Bankacılık-Sistemi/gecmis.h
@@ -1,5 +1,7 @@
 //Abdülselam İsmail 23181616010
-//Bu test amaçlı kaba-taslak bir fonksiyon
+//Bu pragramımızda yapılan işlemleri zaman ve tarihi ile bir dosyaya yazılması sağlanıyor
+
+//ilk önce gereken header dosyalar eklendi
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,13 +10,26 @@
 
 
 
+//Burda gecmis() fonksiyonumuz tanımlandı
+void gecmis(char *islem[100]) {
 
-void gecmis(char *islem[20]) {
+    //<time.h> header' ait olan time_t şeklinde "zaman" belirledik
+    //Ve ona time() fonksiyonu kullanarak zaman değerini veriyoruz
     time_t zaman = time(NULL);
+
+    //ctime() fonksiyonu kullanarak zaman değerimizi okunabilir hale getiriyoruz ve string_zaman string(karakter dizi)'ye atıyoruz
     char *string_zaman = ctime(&zaman);
+
+    //Dosya ile işlem yapailmek gecmiş() pointeri tanımlandı
     FILE *gecmis;
-    gecmis = fopen("C:\\Users\\rist0\\CLionProjects\\ders\\proje\\logs\\gecmis.txt","w");
+
+    //gecmis pointerimize dosyanızı açıyoruz ve "w" kullanarak yazma metodu ile açıyoruz
+    gecmis = fopen("gecmis.txt","w");
+
+    //fprintf () kullanarak "gecmis.txt" dosyamıza parametre olan islem ile zaman ve tarihimizi yazıyoruz
     fprintf(gecmis, "\n%s: %s", islem, string_zaman);
+
+    //fonksiyonumuz sonlandırmadan önce dosyamızı kapatılıyor
     fclose(gecmis);
 
 }
