cda8f51f30a4265309173371e2897ce21d427441
Bayram Enes Atay <bayrameness4242@gmail.com>
2024-01-02 11:12:31 +0300

    Create ParaYatirma.h

---
+++ Bankacılık-Sistemi/ParaYatirma.h
@@ -0,0 +1,81 @@
+//Bayram Enes Atay 23181616059
+//Para yatirma Fonksıyonu.
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+
+void Paracekme(char kullanici[]);
+
+int main()
+{
+    char kullanici[50];
+
+    printf("Kullanici degerini giriniz(isi,soyisim kucuk ve bitisik):");
+    fgets(kullanici,sizeof(kullanici)+1,stdin);
+    kullanici[strcspn(kullanici,"\n")]=0;
+    Paracekme(kullanici);
+    return 0;
+
+}
+
+void Paracekme(char kullanici[])
+{
+    FILE *dosya;
+    int i;
+    char satir5[30];
+    int bakiye;
+    int para;
+
+    dosya=fopen(strcat(kullanici,".txt"),"r");
+
+    if(dosya==NULL)
+    {
+        perror("Error");
+        printf("Error number:%d",errno);
+    }
+    else
+    {
+        for(i=0;i<5;i++)
+        {
+            fgets(satir5,sizeof(satir5)+1,dosya);
+        }
+        fseek(dosya, 0, SEEK_CUR);
+        sscanf(satir5,"%*s%*c%*c%*c%d%*c%*s",&bakiye);
+        printf("Yatirmak istediginiz parayi giriniz:");
+        scanf("%d",&para);
+
+        bakiye+=para;
+        printf("Toplam bakiyeniz: %d",bakiye);
+
+        fseek(dosya,0,SEEK_SET);
+        char cumle[30],cumle2[30],cumle3[30],cumle4[30];
+
+        fgets(cumle,30,dosya);
+        cumle[strcspn(cumle,"\n")]=0;
+        fgets(cumle2,30,dosya);
+        cumle2[strcspn(cumle2,"\n")]=0;
+        fgets(cumle3,30,dosya);
+        cumle3[strcspn(cumle3,"\n")]=0;
+        fgets(cumle4,30,dosya);
+        cumle4[strcspn(cumle4,"\n")]=0;
+        fclose(dosya);
+
+        FILE *dosya2;
+        dosya2=fopen(kullanici,"w");
+        if(kullanici==NULL)
+        {
+            perror("Error");
+            printf("Error number : %d",errno);
+        }
+        else
+        {
+            fprintf(dosya2,"%s\n",cumle);
+            fprintf(dosya2,"%s\n",cumle2);
+            fprintf(dosya2,"%s\n",cumle3);
+            fprintf(dosya2,"%s\n",cumle4);
+            fprintf(dosya2,"Bakiye : %d TL",bakiye);
+            fclose(dosya2);
+        }
+    }
+}
